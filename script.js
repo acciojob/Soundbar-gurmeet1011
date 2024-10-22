484e88cdafe2a604f9b51364160d6bb3ea2a986e
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-22 15:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,27 @@
-//your JS code here. If required.
-// Select all buttons with class 'btn'
-const buttons = document.querySelectorAll('.btn');
-// Stop button
+const buttons = document.querySelectorAll('.btn:not(.stop)');
 const stopButton = document.querySelector('.stop');
-
-// Store the currently playing audio
-let currentAudio = null;
-
-// Add event listeners to all buttons to play respective sounds
+let currentAudio;
+ 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        // Get the sound name from the data attribute
-        const soundName = button.getAttribute('data-sound');
-        // Path to the sound file
-        const soundPath = `./C:/Users/gurme/Desktop/sounds/${soundName}.mp3`;
-
-        // Stop the current audio if playing
-        if (currentAudio) {
-            currentAudio.pause();
-            currentAudio.currentTime = 0;
-        }
-
-        // Create a new audio object and play the sound
-        currentAudio = new Audio(soundPath);
-        currentAudio.play();
+        const sound = button.getAttribute('data-sound');
+        playSound(sound);
     });
 });
+ 
+stopButton.addEventListener('click', stopSound);
+ 
+function playSound(sound) {
+    stopSound(); 
+ 
+ 
+    currentAudio = document.createElement('audio');
+    currentAudio.src = ./sounds/${sound}.mp3;
+    currentAudio.setAttribute('controls', 'controls'); 
+    currentAudio.style.display = 'none'; 
+    document.body.appendChild(currentAudio);
+ 
+    currentAudio.play();
+}
+ 
 
-// Stop the currently playing sound when the stop button is pressed
-stopButton.addEventListener('click', () => {
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0; // Reset audio to start
-        currentAudio = null; // Clear current audio
-    }
-});
